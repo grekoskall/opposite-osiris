db5c34b184eb90ee9aac848d10af259086ca7187
grekoskall <grekoskall@gmail.com>
2024-06-18 22:09:20 +0300

    !fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://grekoskall.github.io',
-  base: 'opposite-osiris'
+  site: 'https://grekoskall.github.io'
 });
